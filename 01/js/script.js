b9e39648e0245e117b9a1296a53cc6d28a221316
seonyoung <kl456694845@gmail.com>
2023-11-30 13:11:28 +0900

    함수로 변경 1차

--- 01/js/script.js
+++ 01/js/script.js
@@ -4,31 +4,26 @@ $(function () {
   const $tabContents = $('.tab-con-item');
 
   console.log($tabMenu, $tabContents);
-
-  // 초기 세팅 : 탭 콘텐츠가 첫번째만 빼고 다 숨겨져 있어야 함
-  $tabContents.hide();
-  $tabContents.eq(0).show();
-
-  // 초기 세팅 : 첫번째 탭메뉴에 on클래스 부여
-  $tabMenu.eq(0).addClass('on');
+  // 초기 세팅
+  tabAction();
 
   // 탭메뉴를 클릭했을 때
   $tabMenu.on('click', function () {
-    // 모든 탭메뉴에서 on클래스 삭제
     $tabMenu.removeClass('on');
 
-    // 클릭한 캡메뉴에 on클래스 부여 --> 인덱스 이용
-
-    // 클릭한 메뉴의 인덱스를 구해서 : index()
     let idx = $(this).index();
-
-    // 그 인덱스에 해당하는 메뉴에 on클래스 부여
     $tabMenu.eq(idx).addClass('on');
 
-    // 모든 탭 콘텐츠를 다 숨기고
     $tabContents.hide();
-
-    // 클릭한 탭메뉴 인덱스에 해당하는 탭 콘텐츠만 보이게
     $tabContents.eq(idx).show();
   });
+
+  // 중복 동작을 함수로 정의
+  function tabAction() {
+    $tabContents.hide();
+    $tabContents.eq(0).show();
+
+    $tabMenu.removeClass('on');
+    $tabMenu.eq(0).addClass('on');
+  }
 });
