afdba6904520cd58af0c1ff561ecc64ff344a039
seonyoung <kl456694845@gmail.com>
2023-12-01 10:44:13 +0900

    배경이미지 적용

--- 02/js/script.js
+++ 02/js/script.js
@@ -10,6 +10,10 @@ $(function () {
   const $tabMenu = $('.tab-menu > li');
   const $tabContents = $('.tab-con-item');
 
+  // 배경이미지를 배열에 저장
+  const bgArr = ['blackpink01.jpg', 'blackpink02.jpg', 'blackpink03.jpg'];
+  console.log(bgArr[2]);
+
   // 초기화
   let idx = 0;
   tabAction(idx);
@@ -18,9 +22,15 @@ $(function () {
   function tabAction(index) {
     // 메서드 체인 : 같은 이름을 지움으로써 코드가 더 간결해짐
     $tabMenu.removeClass('on').eq(index).addClass('on');
-
     $tabContents.hide().eq(index).show();
+
+    // 배경이미지 적용
+    $body.css({
+      // background: 'url(img/' + bgArr[index] + ') no-repeat 0 0/cover',
+      background: `url(img/${bgArr[index]}) no-repeat 0 0 /cover`,
+    });
   }
+
   $tabMenu.on('click', function (e) {
     e.preventDefault(); /* 사이트 화면 주소창에 #이 안뜸 */
 
